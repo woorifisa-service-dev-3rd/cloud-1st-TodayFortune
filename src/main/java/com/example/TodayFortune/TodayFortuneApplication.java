9602d17c5f5bb12acef38abd8221a21d7a594d6e
jihyuk0414 <dealon2580@naver.com>
2024-10-10 10:21:40 +0900

    FIRST-COMMIT-BAKCEND

---
+++ src/main/java/com/example/TodayFortune/TodayFortuneApplication.java
@@ -0,0 +1,13 @@
+package com.example.TodayFortune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayFortuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayFortuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TodayFortune/TodayFortuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TodayFortune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayFortuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
